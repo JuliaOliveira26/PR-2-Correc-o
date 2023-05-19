8bd5150b7ca543357b38763d84401b0bf935d67c
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-05-19 12:01:10 -0300

    dsd

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {livros} from '@/_data/livros.js'
+
+</script>
+
+<template>
+  <h1>Minha Livraria</h1>
+  <div class="listagem-livros">
+    <h2>Livros disponíveis</h2>
+    <div class="card-livro" v-for="livro in livros" :key="livro.id"></div>
+    <img src="livro.img" alt="Capa do Livro" class="capa-livro">
+    <div class="card-livro" v-for="livro in livros" :key="livro.id"> 
+      <p class="titulo-livro">{{ livro.title }} </p>
+      <p class=" autor-livro">{{ livro.author }}</p>
+      <p class="preco-livro">{{ livro.rice }}</p>
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+.listagem-livros{
+  display: flex;
+  flex-wrap: wrap;
+}
+.card-livro{
+  margin: 5px 10px;
+  padding: 10px;
+  background-color: cornflowerblue;
+  border-radius:  10px;
+}
+
+.capa-livro p{
+  margin: 0;
+}
+.card-livro .titulo-livro{
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,45 @@
+ export const livros = [
+    {
+      id: 1,
+      title: 'O poderoso chefão',
+      author: 'Mario Puzo',
+      rice: 49.9
+      
+      
+    },
+    {
+      id: 2,
+      title: 'O senhor dos anéis',
+      author: 'hjdfhdjh',
+      rice: 69.9,
+    
+    },
+    {
+      id: 3,
+      title: 'O conde de monte Cristo',
+      author: 'Alexandre dumas',
+      rice: 59.9,
+    },
+    {
+      id: 4,
+      title: 'O hobbit',
+      author: 'J.R.R.Tolkien',
+      rice: 49.9
+    },
+    {
+      id: 5,
+      title: 'A culpa é das estrelas',
+      author: 'Jhon Green',
+      rice: 49.9,
+    
+    },
+    {
+      id: 6,
+      title: 'Como eu era antes de você',
+      author: 'Jojo Moyes',
+      rice: 69.9
+  
+    }
+  
+  ]
+  
